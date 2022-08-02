546565f189d28b4643b14e92abc8a4dc43ecda34
Redalebiad1 <107723311+Redalebiad1@users.noreply.github.com>
2022-08-02 19:25:49 +0100

    Add files via upload

---
+++ Starbucks/js/script.js
@@ -0,0 +1,7 @@
+function imgSlider(cup){
+    document.querySelector('.starbucks').src = cup;
+}
+function changeCircleColor(color){
+    const circle = document.querySelector('.circle');
+    circle.style.background = color;
+}
